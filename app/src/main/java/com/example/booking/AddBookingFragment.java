f6febc20a17a76ec59cf2a49100c6275c893f529
Tim Kainz <kainzt21030@sus.htl-grieskirchen.at>
2024-03-04 16:41:12 +0100

    initial commit

---
+++ app/src/main/java/com/example/booking/AddBookingFragment.java
@@ -0,0 +1,40 @@
+package com.example.booking;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.booking.databinding.FragmentAddBookingBinding;
+
+public class AddBookingFragment extends Fragment {
+    FragmentAddBookingBinding binding;
+    public AddBookingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAddBookingBinding.inflate(inflater,container,false);
+        binding.tbAdd.setTitle("Neue Buchung");
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.tbAdd.inflateMenu(R.menu.menu_add);
+    }
+
+}
